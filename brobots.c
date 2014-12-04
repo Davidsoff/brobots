cf8aeae519fd784e8bf0c4866e9a2db17912b6ab
Roy Buitenhuis <roy.buitenhuis94@gmail.com>
2014-12-04 17:14:10 +0100

    Got it make worked.

--- brobots.c
+++ brobots.c
@@ -1,55 +1,57 @@
 
 
 #include "simpletools.h"                      // Include simple tools
+#include "servo.h"
+#include "ping.h"
 
-void ping(void *par);
-                       // Forward declaration
+void pingThread(void* par);
+
+int checkFront();
+int checkRight();
 
 static volatile int sonicFront, sonicRight;    // Global vars for cogs to share
-//unsigned int stack1[40 + 25];                  // Stack vars for other cog
+unsigned int stack[40 + 25];                  // Stack vars for other cog
 
 int main()                                    // main function
 {
-
-
-  // Launch adder function into another cog (processor).
-  //cogstart(&ping, NULL, stack1, sizeof(stack1));
-
-  // Watch what the other cog is doing to the value of n.
-  while(1)
-  {
-    servo_angle(16,1000);
-    sonicRight = ping_cm(17);
-    servo_angle(16,2050);
-    sonicRight = ping_cm(17);
-
-    print("front = %d cm\n", sonicFront);
-    print("right = %d cm\n", sonicRight);                    // Display result
-    pause(100);                               	// Wait 1/10 of a second
-  }
+    
+    servo_start();
+    // Launch adder function into another cog (processor).
+    //cogstart(&ping, NULL, stack1, sizeof(stack1));
+    print("hoi");
+    // Watch what the other cog is doing to the value of n.
+    cogstart(&pingThread, NULL, stack, sizeof(stack));
+    while(1)
+    {
+        // Display result
+        print("data: \n");
+        pause(1000);
+        print("Front %d\n", sonicFront);
+        print("Right %d\n" , sonicRight );                              	// Wait 1/10 of a second
+    }
 }
 
 // Function that can continue on its
 // own if launched into another cog.
-void ping(void *par)                         // adder keeps going on its own
+void pingThread(void* par)                         // adder keeps going on its own
 {
-  while(1)                                    // Endless loop
-  {
-    checkFront();
-    pause(100);
-    checkRight();                                // n + 1 each time loop repeats
-    pause(100);                                 // Wait for t ms between updates
-  }
+    while(1)                                    // Endless loop
+    {
+        sonicFront = checkFront();
+        pause(300);
+        sonicRight = checkRight();
+        pause(300);                              // Wait for t ms between updates
+    }
 }
 
-void checkRight(){
-  //turn servo to the right
-  servo_angle(16,1000);
-  sonicRight = ping_cm(17);
-}
-void checkFront(){
- //turn servo to front
-  servo_angle(16,2050);
-  sonicRight = ping_cm(17);
+int checkFront(){
+    servo_angle(16, 2050);
+    pause(500);
+    return ping_cm(17);
 }
 
+int checkRight(){
+    servo_angle(16, 1000);
+    pause(500);
+    return ping_cm(17);
+}
