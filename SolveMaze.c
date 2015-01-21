8e8acefc3e9530a52126a53fee8312fd01022501
Terry van der Jagt <Terryvanderjagt@users.noreply.github.com>
2015-01-21 18:41:35 +0100

    Finally its working great

--- SolveMaze.c
+++ SolveMaze.c
@@ -3,9 +3,24 @@
 #include "ping.h"
 #include "abdrive.h"
 
+
+
+//PARAMETERS
+
+const int rightPingPort = 16;
+const int frontPingPort = 2;
+const int bumperPort = 6;
+
+const int maxRightTurns = 8;
+const int resetRightTurnsOnState1 = 0;
+
+
+
+
 volatile static int distance[2];
 volatile static int state;
-volatile static int left_turn_count;
+volatile static int leftTurnCount;
+volatile static int  rightTurnCount;
 unsigned int scannerStack[40+25];
 unsigned int stateStack[40+25];
 
@@ -19,14 +34,19 @@ void escape();
 void stop();
 void turn_right();
 void turn_left(int iter);
+void startup();
 
 int main(){
   //initialisation
-  int mode = 0;
   state = -2;
-  left_turn_count = 0;
+  leftTurnCount = 0;
+  rightTurnCount = 0;
+  
+  //Start cogs
   cogstart(&updateDistanceCog, NULL, scannerStack, sizeof(scannerStack));
   cogstart(&driveStateCog, NULL, stateStack, sizeof(stateStack));
+  pause(2000);
+ 
  
   //main program loop
   while(1){
@@ -34,23 +54,25 @@ int main(){
     int right = distance[RIGHT];
     int front = distance[FRONT];
     
-    int bumper = input(6);
+    int bumper = input(bumperPort);
     if(bumper == 0){
-      state = 4;
+      state = 4;  //Bumper is pressed
     }
     else
     {      
 
-      if(right > 10){
-        state = 2;     
-      }else if(right < 10 && front <= 3 ){
-        state = 3;
-      }else if(front>3){
-        state = 1;
+      if((right > 10)&&(right < 200)){
+        state = 2; //Right turn 
+           
+      }else if(right < 10 && front <=  6){
+        state = 3; //Left turn
+        
+      }else if(front>6){
+        state = 1; //Drive forward
       
-      //Rechts grote afstand, er kan een bocht worden gemaakt, deze functie wordt aangeroepen
       }else{
-          state = -1;    
+          state = -1; //Shouldn't be possible
+             
       }
     }   
   }  
@@ -59,9 +81,9 @@ int main(){
 
 void updateDistanceCog(){
   while(1){
-    distance[FRONT] = ping_cm(2);
+    distance[FRONT] = ping_cm(frontPingPort);
     pause(6);
-    distance[RIGHT] = ping_cm(16);
+    distance[RIGHT] = ping_cm(rightPingPort);
     pause(6);
     
   }  
@@ -73,35 +95,54 @@ void driveStateCog(){
       case -2:
         
         startup();
+        leftTurnCount=0;
+        rightTurnCount=0;
        break;
                   
       
       case -1:
-        left_turn_count = 0;
+        //Too close to the wall
+        leftTurnCount = 0;
         escape();
         state = 0;
       break;
+      
       case 0:
         stop();
       break;
+      
       case 1:
-        left_turn_count = 0;
+        //drive forward
+        leftTurnCount = 0;
+        if(resetRightTurnsOnState1==1){
+          rightTurnCount=0;
+        }
+        
         drive();
       break;
+      
+      
       case 2:
-        left_turn_count = 0;
+        //Turn right
+        leftTurnCount = 0;
         turn_right();
+        rightTurnCount++;
         state = 0;
       break;
+      
       case 3:
-        turn_left(left_turn_count++);
+        turn_left(leftTurnCount++);
+        rightTurnCount=0;
         state = 0;
       break;
+      
       case 4:
         escape_bumper();
         state = 0;
+      break;
+      
       default:
-        left_turn_count = 0;
+        leftTurnCount = 0;
         state = 0;
       break;
     }
@@ -109,13 +150,14 @@ void driveStateCog(){
   }  
 }  
 
-//robot rijd naar voren
+//robot drives forward
 void drive(){
-  drive_speed(20, 20);
+  drive_speed(30, 30);
 }
 
+//robot drives a bit backwards
 void escape(){
-  high(26);
+  high(26);//status led
   drive_goto(-5,-5);
   low(26);
 }
@@ -124,19 +166,26 @@ void stop(){
   drive_speed(0,0);
 }
 
+//bumper is pressed
 void escape_bumper(){
   escape();
   drive_goto(-3, 3); 
 }  
 
-//robot draait 90 graden naar rechts          
+//robot turns 90 degrees to the right          
+     
 void turn_right(){
+  if((maxRightTurns>0)&&(rightTurnCount>maxRightTurns)){
+    startup();
+    return;
+  }    
+  
   drive_goto(12, 12);
   drive_goto(26, -25);
   drive_goto(50, 50);
 }
 
-//robot draait 90 graden naar links
+//robot turns 90 degrees to the left
 void turn_left(int iter){
   if(iter == 0)
     drive_goto(-5, -5);
